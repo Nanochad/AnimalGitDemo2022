7d2cdc47f593bd53e4dfd4098fd7256d2f2ca039
Rodrigo <rn.patricio@campus.fct.unl.pt>
2023-09-22 18:02:04 +0100

    Added Dogs Cats and updated main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main (String[] Args){
-        System.out.println("Hello World");
-    }
+
+        public static void main(String[] args) {
+            System.out.println("Let's hear it from the pets");
+            Animal dog = new Dog("Bobby");
+            Animal cat = new Cat("Kitty");
+            System.out.println(dog.getName() + " says " + dog.speak());
+            System.out.println(cat.getName() + " says " + cat.speak());
+        }
+
 }
